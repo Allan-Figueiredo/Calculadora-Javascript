f030772c206af7ad47ef87c86a095b35017e086f
AllanFGR <115052566+Allan-Figueiredo@users.noreply.github.com>
2024-02-08 17:34:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function outcome(){
     let num1 = Number(document.getElementById('num-um').value)
     let num2 = Number(document.getElementById('num-dois').value)
     let total = 0
-    //
+    
     if(document.getElementById('caixa1').checked)
     total = num1 + num2
     else if ((document.getElementById('caixa2').checked))
